0b7ba85eb1b8f66d8ca268592a5cbe5ca7e25705
Wrecked <284432863@qq.com>
2018-07-05 17:08:27 +0800

    添加AtomicInteferDemo

--- src/main/java/com/puzzle/atomic/AtomicIntegerDemo.java
+++ src/main/java/com/puzzle/atomic/AtomicIntegerDemo.java
@@ -1,9 +1,51 @@
 package com.puzzle.atomic;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by Administrator on 2018/7/5 0005.
  */
 public class AtomicIntegerDemo {
+    static CountDownLatch countDownLatch = new CountDownLatch(1000);
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
+    static int count = 0;
 
+    public static void main(String[] args) throws InterruptedException {
+
+
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("market-pool-%d").build();
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(100, 200, 0, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>(), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+        for (int i = 0; i < 1000; i++) {
+            threadPoolExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    atomicInteger.getAndIncrement();
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    count ++;
+                    countDownLatch.countDown();
+
+                }
+            });
+        }
+        countDownLatch.await();
+        System.out.println("线程安全的AtmicInteger" + atomicInteger.get());
+        System.out.println("非线程安全的Integer" + count);
+        System.exit(1);
+
+
+    }
 
 }
